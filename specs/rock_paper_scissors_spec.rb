bc1b6b5636fb4568e42b3d9cdce9e397ac267d99
Donald Cameron <user@Users-MBP-5.lan>
2020-05-25 20:34:48 +0100

    tried to fix spec file, not sure how to test against something random

--- specs/rock_paper_scissors_spec.rb
+++ specs/rock_paper_scissors_spec.rb
@@ -4,39 +4,13 @@ require_relative '../models/rock_paper_scissors.rb'
 class TestRock_Paper_Scissors < Minitest::Test
 
   def test_rock
-    assert_equal("Computer wins the game!", Rock_Paper_Scissors.play("rock") )
-  end
-
-  def test_rock
-    assert_equal("It's a draw", Rock_Paper_Scissors.play("rock") )
-  end
-
-  def test_rock
-    assert_equal("You win the game", Rock_Paper_Scissors.play("rock") )
+    assert_equal("rock", Rock_Paper_Scissors.play("rock") )
   end
 
   def test_paper
-    assert_equal("Computer wins the game!", Rock_Paper_Scissors.play("paper") )
-  end
-
-  def test_paper
-    assert_equal("It's a draw", Rock_Paper_Scissors.play("paper") )
-  end
-
-  def test_paper
-    assert_equal("You win the game", Rock_Paper_Scissors.play("paper") )
+    assert_equal("paper", Rock_Paper_Scissors.play("paper") )
   end
 
   def test_scissors
-    assert_equal("Computer wins the game!", Rock_Paper_Scissors.play("scissors") )
+    assert_equal("scissors", Rock_Paper_Scissors.play("scissors") )
   end
-
-  def test_scissors
-    assert_equal("It's a draw", Rock_Paper_Scissors.play("scissors") )
-  end
-
-  def test_scissors
-    assert_equal("You win the game", Rock_Paper_Scissors.play("scissors") )
-  end
-
-end
